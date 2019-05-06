482e8ea6c4300bf5e349a7b59a7e99de298b66a8
Mikkel Boese Johnsen <mikk571a@edu.campusvejle.dk>
2019-05-06 09:41:34 +0200

    starter forfra uden Quasar af Klement årsager

--- src/main.js
+++ src/main.js
@@ -1,50 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import './styles/quasar.styl'
-import 'quasar/dist/quasar.ie.polyfills'
-import '@quasar/extras/roboto-font/roboto-font.css'
-import '@quasar/extras/material-icons/material-icons.css'
-import {
-  Quasar, 
-  QLayout,
-  QHeader,
-  QDrawer,
-  QPageContainer,
-  QPage,
-  QToolbar,
-  QToolbarTitle,
-  QBtn,
-  QIcon,
-  QList,
-  QItem,
-  QItemSection,
-  QItemLabel,
-} from 'quasar'
-
-Vue.use(Quasar, {
-  config: {},
-  components: {
-    QLayout,
-    QHeader,
-    QDrawer,
-    QPageContainer,
-    QPage,
-    QToolbar,
-    QToolbarTitle,
-    QBtn,
-    QIcon,
-    QList,
-    QItem,
-    QItemSection,
-    QItemLabel,
-  },
-  directives: {
-  },
-  plugins: {
-  }
- })
-
 Vue.config.productionTip = false
 
 new Vue({
